bb334551ce696507c0444d4a6850eadbf06410b1
ashiful-islam <ashifulislam2002@gmail.com>
2024-12-10 21:15:33 +0600

    edited projects.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,37 +1,10 @@
 import React from "react";
-import Section from "../Components/Title.jsx/Section";
-import heroImage from "../assets/hero.jpg";
-import { aboutDetails } from "../Constants/Index";
 import Projects from "./Section/Projects";
+import Hero from "./Section/Hero";
 const Home = () => {
   return (
     <>
-    <Section>
-      <h1 className="heading">home page</h1>
-      <div>
-        <div className="flex items-center justify-center flex-col xl:flex-row  bg-slate-600 rounded  text-neutral-200 p-4">
-          <div className="xl:w-1/2">
-            <img
-              src={heroImage}
-              alt="heroImage"
-              srcset=""
-              className="rounded-xl p-2 w-[450px]"
-            />
-          </div>
-
-          <div className="xl:w-1/2">
-            {aboutDetails.map((item, index) => (
-              <div key={index} className="mt-4">
-                <h2 className="text-capitalize">{item.name}</h2>
-                <h5>{item.title}</h5>
-                <p className="text-justify tracking-tight">{item.desc}</p>
-              </div>
-            ))}
-          </div>
-        </div>
-      </div>
-     
-    </Section>
+   < Hero/>
     <Projects />
     </>
   );
--- src/pages/Section/Projects.jsx
+++ src/pages/Section/Projects.jsx
@@ -1,54 +1,54 @@
-import React from 'react'
-import Section from '../../Components/Title.jsx/Section'
-import PageTitle from "../../Components/Title.jsx/PageTitle"
+import React from "react";
+import Section from "../../Components/Title.jsx/Section";
+import PageTitle from "../../Components/Title.jsx/PageTitle";
 const Projects = () => {
   return (
-    <Section>
-      < PageTitle />
+    <Section id="projects">
+      <PageTitle heading="projects" />
       <div>
-      <div className="">
-        <div className="grid grid-cols-1 md:grid-cols-3 gap-4 border rounded">
-          <div>
-            {" "}
-            <iframe
-              src="https://engineershome.vercel.app/"
-              width="375px"
-              height="500px"
-              frameborder="0"
-            ></iframe>
-          </div>
-          <div>
-            {" "}
-            <iframe
-              src="https://helloshe.org/"
-              width="375px"
-              height="500px"
-              frameborder="0"
-            ></iframe>
-          </div>
-          <div>
-            {" "}
-            <iframe
-              src="https://photoholicah.vercel.app/"
-              width="375px"
-              height="500px"
-              frameborder="0"
-            ></iframe>
-          </div>
-          <div>
-            {" "}
-            <iframe
-              src="https://rswa-rowmari.vercel.app/"
-              width="375px"
-              height="500px"
-              frameborder="0"
-            ></iframe>
+        <div className="">
+          <div className="grid grid-cols-1 md:grid-cols-3 gap-4 border rounded">
+            <div className="border rounded-md shadow m-2 mb-5">
+              {" "}
+              <iframe
+                src="https://engineershome.vercel.app/"
+                width="375px"
+                height="500px"
+                frameborder="0"
+              ></iframe>
+            </div>
+            <div className="border rounded-md shadow m-2 mb-5">
+              {" "}
+              <iframe
+                src="https://helloshe.org/"
+                width="375px"
+                height="500px"
+                frameborder="0"
+              ></iframe>
+            </div>
+            <div className="border rounded-md shadow m-2 mb-5">
+              {" "}
+              <iframe
+                src="https://photoholicah.vercel.app/"
+                width="375px"
+                height="500px"
+                frameborder="0"
+              ></iframe>
+            </div>
+            <div className="border rounded-md shadow m-2 mb-5">
+              {" "}
+              <iframe
+                src="https://rswa-rowmari.vercel.app/"
+                width="375px"
+                height="500px"
+                frameborder="0"
+              ></iframe>
+            </div>
           </div>
         </div>
       </div>
-      </div>
     </Section>
-  )
-}
+  );
+};
 
-export default Projects
+export default Projects;

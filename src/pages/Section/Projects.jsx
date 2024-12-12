ba4a3f67e6777cc4b50d4f443bb7f2605e64c7c9
ashiful-islam <ashifulislam2002@gmail.com>
2024-12-13 02:10:24 +0600

    added Features on Projects

--- src/pages/Section/Projects.jsx
+++ src/pages/Section/Projects.jsx
@@ -8,21 +8,19 @@ const Projects = () => {
       <PageTitle heading="projects" />
       <div>
         <div className="">
-         
           <div className="grid grid-cols-1 md:grid-cols-3 gap-2 border rounded">
-          <p className="px-2">There are some projects i've recently done</p>
-            {
-              projects.map((item)=>(
-                <div className="border rounded-md shadow m-2 mb-2" key={item.id}>
-                
+            <p className="px-2">There are some projects i've recently done</p>
+            {projects.map((item) => (
+              <div className="border rounded-md shadow m-2 mb-2" key={item.id}>
                 <a href={item.url} target="_blank" rel="noopener noreferrer">
-                <img src={item.img} alt={item.title} className="p-2" /></a>
-                <h2 className="px-2">{item.title}</h2>
-                <p className="px-2">{item.desc}</p>
+                  <img src={item.img} alt={item.title} className="p-2" /></a>
+                  <div className="">
+                  <h2 className="px-2">{item.title}</h2>
+                  <p className="px-2">{item.desc}</p>
+                  </div>
+                
               </div>
-              ))
-            }
-           
+            ))}
           </div>
         </div>
       </div>

4fe893820535739b3ee0c3da8b3896499e50d31a
ashiful-islam <ashifulislam2002@gmail.com>
2024-12-19 23:50:57 +0600

    Added basic todo App

--- src/Components/IconCard.jsx
+++ src/Components/IconCard.jsx
@@ -1,18 +1,17 @@
-import React from "react";
-import { FaHome, FaUser, FaCog, FaBell } from "react-icons/fa";
-import { icons } from "../constants/icons";
+// import React from "react";
+// import { icons } from "../constants/icons";
 
-const IconCards = () => {
-  return (
-    <div className="flex gap-4 flex-wrap">
-      {icons.map(({ id, name, icon: Icon }) => (
-        <div key={id}>
-          <Icon size={40} className="tetx" />
-          <p>{name}</p>
-        </div>
-      ))}
-    </div>
-  );
-};
+// const IconCards = () => {
+//   return (
+//     <div className="flex gap-4 flex-wrap">
+//       {icons.map(({ id, name, icon: Icon }) => (
+//         <div key={id}>
+//           <Icon size={40} className="tetx" />
+//           <p>{name}</p>
+//         </div>
+//       ))}
+//     </div>
+//   );
+// };
 
-export default IconCards;
+// export default IconCards;
--- src/Constants/icons.js
+++ src/Constants/icons.js
@@ -1,8 +1,8 @@
-import { FaHome, FaUser, FaCog, FaBell } from "react-icons/fa";
+// import { FaHome, FaUser, FaCog, FaBell } from "react-icons/fa";
 
-export const icons = [
-  { id: 1, name: "home", icon: FaHome },
-  { id: 2, name: "user", icon: FaUser },
-  { id: 3, name: "settings", icon: FaCog },
-  { id: 4, name: "notifications", icon: FaBell },
-];
+// export const icons = [
+//   { id: 1, name: "home", icon: FaHome },
+//   { id: 2, name: "user", icon: FaUser },
+//   { id: 3, name: "settings", icon: FaCog },
+//   { id: 4, name: "notifications", icon: FaBell },
+// ];

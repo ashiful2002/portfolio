efdd8305e5781a015e113b330b134c7593cf62c6
ashiful-islam <ashifulislam2002@gmail.com>
2025-01-11 23:49:37 +0600

    Removed Dodnloadable File form resume button

--- src/pages/Home/Section/Hero.jsx
+++ src/pages/Home/Section/Hero.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 
-import mealcounter from "../../../assets/mealCounter.pdf"
 import heroImage from "../../../assets/hero.jpg";
 import heroImage2 from "../../../assets/heroimage2.jpg";
 import heroImage3 from "../../../assets/heroimage3.png";
@@ -29,7 +28,7 @@ const Hero = () => {
                 <h2 className="text-capitalize">{item.name}</h2>
                 <h5>{item.title}</h5>
                < >
-               <Button className="mt-0 text-sm" ><a href="" className="no-underline text-white" download={mealcounter}>
+               <Button className="mt-0 text-sm" ><a href="" className="no-underline text-whitew" >
                Download Resume</a></Button>
                </>
                 <p className="text-justify tracking-tight">{item.desc}</p>
